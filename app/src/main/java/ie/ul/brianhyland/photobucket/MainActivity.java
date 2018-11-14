22228ad9ba6f3a9e6e1328beed620e211ce3191c
Brian Hyland <39333309+hylbrian@users.noreply.github.com>
2018-11-14 16:56:18 +0000

    TESTING FIREBASE wrong place for now

--- app/src/main/java/ie/ul/brianhyland/photobucket/MainActivity.java
+++ app/src/main/java/ie/ul/brianhyland/photobucket/MainActivity.java
@@ -1,15 +1,31 @@
 package ie.ul.brianhyland.photobucket;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG ="PB";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +34,41 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //Temp 
+        /*Temp firebase testing area
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("imageUrl")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (DocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+                */
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Added firestore doc", Snackbar.LENGTH_LONG).show();
+
+                /* Create a new user with a first and last name
+                Map<String, Object> pb = new HashMap<>();
+                pb.put("caption", "Test picture");
+                pb.put("imageUrl", "Test URL");
+
+
+// Add a new document with a generated ID
+                db.collection("imageUrl").add(pb);
+                */
+
             }
         });
     }
